8f1e20a694b25798a299ef01e9d57dc7d8ce6fd2
SignorPao <91122227+SignorPao@users.noreply.github.com>
2022-04-19 13:59:16 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var nav = document.querySelector('nav');
+
+window.addEventListener('scroll', function(){
+    if(window.pageYOffset > 10){
+        nav.classList.add('bg-dark', 'shadow');
+    }else{
+        nav.classList.remove('bg-dark', 'shadow');
+    }
+});
+
